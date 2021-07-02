5943cd9dda480d122b9d3606e222e3008a42521b
GabrielSilva88 <gabrielmg.silva@outlook.com>
2021-07-02 17:22:20 -0300

    NewProject ContextApi

---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
+
+class TodoList extends React.Component {
+    static contextType = ThemeContext;
+    //static contextType = AuthContext;
+    render() {
+        const { isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        return (
+            <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center' }}>
+                <p className='item'>Plan the Family trip</p>
+                <p className='item'>Go for shopping for dinner</p>
+                <p className='item'>Go for a walk</p>
+                {
+                    //<p className='item'>Go for Gabriel Miguel </p>
+                }
+                <button className='ui button primary' onClick={changeTheme}>change the theme</button>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
